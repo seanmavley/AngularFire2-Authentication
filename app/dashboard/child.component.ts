fe9aba6ea3c6f8a0f2331f3bb081ec812fbb9838
Rexford <seanmavley@gmail.com>
2016-10-04 12:01:02 +0000

    revert to 3.4.0 firebase. Including authrelated pages

--- app/dashboard/child.component.ts
+++ app/dashboard/child.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   selector: 'profile',
   templateUrl: 'app/dashboard/profile.component.html'
 })
 
-export class ProfileComponent { }
+export class ProfileComponent { 
+    public userData: any;
+    constructor(private af: AngularFire) {  }
+
+    ngOnInit() {
+        this.af.auth.subscribe(auth => {
+          console.log(auth);
+          this.userData = auth;
+        });
+    }
+
+}
 
 @Component({
   selector: 'profile',
   templateUrl: 'app/dashboard/account.component.html'
 })
 
-export class AccountComponent { }
+export class AccountComponent { 
+
+  onSubmit(formData) {
+    if(formData.valid) {
+      console.log(formData.value);
+    }
+  }
+
+}
 
 @Component({
   selector: 'profile',
